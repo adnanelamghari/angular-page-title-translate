01a20dc396f71daf11c3bd629a5bf371a128bf40
Adnane Lamghari <adnane.lmg@gmail.com>
2022-07-03 19:12:26 +0200

    translate page titles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'page1',
+  component: Page1Component,
+  title: 'PAGE_1.TITLE'
+}, {
+  path: 'page2',
+  component: Page2Component,
+  title: 'PAGE_2.TITLE'
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
+import {HttpClient, HttpClientModule} from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {TitleStrategy} from '@angular/router';
+import {CustomPageTitleStrategy} from './core/custom-page-title-strategy';
+
+function  HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page1Component,
+    Page2Component
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    { provide: TitleStrategy, useClass: CustomPageTitleStrategy }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
